8f52a05a968dfc3be8f67d6cdce69286b89d5e7e
Aiswarya <aiswaryaas2000@gmail.com>
2026-07-25 14:09:38 +0530

    1. Network 2. member screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.example.my_app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 //        versionCode = flutter.versionCode
 //        versionName = flutter.versionName
 
